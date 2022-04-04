0e25d8bf7c714c219df00b1522695e1dc154edae
jsoares07 <jfernandosoares7@gmail.com>
2022-04-04 13:22:28 +0000

    Added the main files

--- src/app.js
+++ src/app.js
@@ -6,6 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var symbols = ["♥", "♠", "♣", "♦"];
+  var content = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  var randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
+  var randomContent = content[Math.floor(Math.random() * content.length)];
+
+  document.querySelector(".card-top").innerHTML = randomSymbols;
+  document.querySelector(".card-content").innerHTML = randomContent;
+  document.querySelector(".card-bot").innerHTML = randomSymbols;
+
+  if (randomSymbols == "♥" || randomSymbols == "♦") {
+    document.querySelector(".card").style.color = "red";
+  } else if (randomSymbols == "♠" || randomSymbols == "♣") {
+    document.querySelector(".card").style.color = "black";
+  }
 };
